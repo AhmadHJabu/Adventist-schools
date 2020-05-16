aa0386bab971a56afbf1114cbe34a1244d64b768
Ahmed Hussein Jabu <ahmadhussein99@yahoo.com>
2020-05-16 10:22:26 +0300

    manifest and push-worker notification

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `d92638f59fc4b715a8648d6010e2015c`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
